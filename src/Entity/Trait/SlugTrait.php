0370529220350c633ac08a480a66854fac9c8d61
DioufFLR <geoffreyfleur@gmail.com>
2023-03-13 15:38:05 +0100

    2.4 migration update composer.json add fixtures

--- src/Entity/Trait/SlugTrait.php
+++ src/Entity/Trait/SlugTrait.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 trait SlugTrait
 {
     #[ORM\column(type: 'string', length: 255)]
-    private $slug;
+    private ?string $slug;
 
     public function getSlug(): ?string
     {
