341df43d4bd71b3485f10f5035ab9cc0133d47cb
DioufFLR <geoffreyfleur@gmail.com>
2023-03-14 15:38:23 +0100

    2. faker users

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -21,6 +21,12 @@ class CategoriesFixtures extends Fixture
         $this->createCategory('Souris', $parent, $manager);
         $this->createCategory('Claviers', $parent, $manager);
 
+        $parent = $this->createCategory('Mode', null, $manager);
+
+        $this->createCategory('Homme', $parent, $manager);
+        $this->createCategory('Femme', $parent, $manager);
+        $this->createCategory('Enfant', $parent, $manager);
+
         $manager->flush();
     }
 
