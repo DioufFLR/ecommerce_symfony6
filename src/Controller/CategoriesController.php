1fc07bd6096ba2fd10ddb56c88c30504a7983302
DioufFLR <geoffreyfleur@gmail.com>
2023-03-17 10:57:28 +0100

    3.4 product details

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Products;
+use App\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,15 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/categories', name: 'categories_')]
 class CategoriesController extends AbstractController
  {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/{slug}', name: 'list')]
+    public function list(Categories $category): Response
     {
-        return $this->render('products/index.html.twig');
-    }
-
-    #[Route('/{slug}', name: 'details')]
-    public function details(Products $product): Response
-    {
-        return $this->render('products/details.html.twig', compact('product'));
+        return $this->render('categories/list.html.twig', compact('category'));
     }
  }
