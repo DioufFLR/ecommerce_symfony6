fdd1e4404ddb36e2affcc48864fc1f8bcc4b193e
DioufFLR <geoffreyfleur@gmail.com>
2023-03-31 11:23:01 +0200

    5.3 upload images

--- src/Service/PictureService.php
+++ src/Service/PictureService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use PHPUnit\Util\Exception;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use function PHPUnit\Framework\fileExists;
 
 class PictureService
 {
@@ -41,6 +42,59 @@ class PictureService
             default:
                 throw new Exception("Format d'image incorrect");
         }
+
+        // On recadre l'image
+        // On récupère les dimensions
+        $imageWidth = $picture_infos[0];
+        $imageHeight = $picture_infos[1];
+
+        // On vérifie l'orientation de l'image
+        switch ($imageWidth <=> $imageHeight){
+            case -1: // portrait
+                $squareSize = $imageWidth;
+                $src_x = 0;
+                $src_y = ($imageHeight - $squareSize) / 2;
+                break;
+            case 0: // carré
+                $squareSize = $imageWidth;
+                $src_x = 0;
+                $src_y = 0;
+                break;
+            case 1: // paysage
+                $squareSize = $imageHeight;
+                $src_x = ($imageWidth - $squareSize) / 2;
+                $src_y = 0;
+                break;
+        }
+
+        // On crée une nouvelle image "vierge"
+        $resize_picture = imagecreatetruecolor($width, $height);
+
+        imagecopyresampled($resize_picture, $picture_source, 0, 0, $src_x, $src_y, $width, $height, $squareSize, $squareSize);
+
+        $path = $this->params->get('images_directory') . $folder;
+
+        // On crée le dossier de destination s'il n'existe pas
+        if (!file_exists($path . '/mini/')){
+            mkdir($path . '/mini/', 0755, true  );
+        }
+
+        // On stocke l'image recadrée
+        imagewebp($resize_picture, $path . '/mini/' . $width . 'x' . $height . '-' . $fichier);
+
+        $picture->move($path . '/', $fichier);
+
+        return $fichier;
     }
 
+    public function delete(string $fichier, ?string $folder = '', ?int $width = 250, ?int $height = 250)
+    {
+        if ($fichier !== 'default.webp'){
+            $success = false;
+            $path = $this->params->get('images_directory') . $folder;
+
+            $mini = $path . '/mini/' . $width . 'x' . $height . '-' . $fichier;
+
+        }
+    }
 }
